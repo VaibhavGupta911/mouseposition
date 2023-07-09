2e44063f7e9ff9a8db7dc25c7e991bc6b5a479d6
VaibhavGupta911 <guptaayush519@gmail.com>
2023-07-09 22:57:35 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
+
+//this is enhancer
+const withMousePosition = (WrappedComponent) => {
+  return (props) => {
+    const [mousePosition, setMousePosition] = useState(
+      {
+        x: 0,
+        y: 0,
+      }
+    )
+    useEffect(
+      () => {
+        const handleMousePosition = (e) => {
+          setMousePosition(
+            {
+              x: e.clientX,
+              y: e.clientY,
+            }
+          );
+        }
+
+        window.addEventListener("mousemove", handleMousePosition);
+        //we have to closed event also
+        return () => {
+          window.removeEventListener("mousemove", handleMousePosition);
+        }
+      }, []
+    );
+    return (<WrappedComponent {...props} mousePosition={mousePosition} />)
+    //we are retuning WrappedCom as enhenced whivh will be assifgned to another com
+  };
+}
+
+
+//this com will be enhanced
+const positionMouseLogger = ({ mousePosition }) => {
+  if (!mousePosition) {
+    return null;
+  }
+  return (
+    <>
+      {mousePosition.x},{mousePosition.y}
+    </>
+  )
+};
+
+//this is higher order component with mouse pointer
+const EnhencedPointer=withMousePosition(positionMouseLogger);
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Mouse Pointer Position</h1>
+      <h3 >(<EnhencedPointer/>)</h3>
     </div>
   );
 }
